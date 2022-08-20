59f3e632ccc67bdf209ccfba477ed379cd089a5b
Alal11 <spalqj56@naver.com>
2022-08-21 01:31:57 +0900

    수정했음

--- 계산기.py
+++ 계산기.py
@@ -5,13 +5,12 @@ operator = {'＋':1, '－':2, '÷':3, '×':4, 'C':5, '＝':6}
 stoValue = 0
 opPre = 0
 
-def number_Click(value):
-    # print('숫자', value)
+def number_Click(value):                         # 숫자 클릭 정의
     global disValue
     disValue = (disValue*10) + value
     str_value.set(disValue)
 
-def clear():
+def clear():                                      #  Clear 정의
     global disValue, operator, stoValue, opPre
     disValue = 0
     stoValue = 0
@@ -19,8 +18,7 @@ def clear():
     str_value.set(disValue)
  
 
-def oprator_Click(value):
-    # print('명령', value)
+def oprator_Click(value):                          # 연산자 클릭 정의
     global disValue, operator, stoValue, opPre
     op = operator[value]
     if op == 5:          # C
@@ -32,7 +30,7 @@ def oprator_Click(value):
         stoValue = disValue
         disValue = 0
         str_value.set(str(stoValue))
-    elif op == 6:        # =
+    elif op == 6:         # =
        if opPre == 1:
            disValue = stoValue + disValue
        if opPre == 2:
@@ -49,9 +47,7 @@ def oprator_Click(value):
         clear()
 
 
-
-def Button_Click(value):
-    # print(value)
+def Button_Click(value):                              # 버튼 클릭 정의
     try:
         value = int(value)
         number_Click(value)
@@ -59,18 +55,20 @@ def Button_Click(value):
         oprator_Click(value)
 
 
+
 win = tk.Tk()                      # 창 생성
 
 win.title("계산기")             # 창 제목
 
-
+# 결과 창 구조
 str_value = tk.StringVar()
 str_value.set(str(disValue))
 dis = tk.Entry(win, textvariable = str_value, justify = 'right', bg = 'whitesmoke', fg = 'black')
-dis.grid(column=0, row=0, columnspan=4, ipadx=90, ipady=60)
+dis.grid(column = 0, row = 0, columnspan = 4, ipadx = 90, ipady = 60)
 
+# 버튼 구조
 Button_list = [['%','CE','C','Back'],
-               ['1/x','x^2','2√x','÷'],
+               ['1/x','x²','²√x','÷'],
                ['7','8','9','×'],
                ['4','5','6','－'],
                ['1','2','3','＋'],
@@ -93,6 +91,7 @@ for i,items in enumerate(Button_list):
             fg = 'black',
             command = lambda cmd = item: Button_Click(cmd)
             )
-        btn.grid(column=k, row=(i+1))
+        btn.grid(column = k, row = (i + 1))
 
 win.mainloop()                  # 창 실행
+
